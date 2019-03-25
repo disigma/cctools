7264d2baac3ab455654bc0205134af08cac14129
disigma <yang.yu@disigma.org>
2019-03-25 12:03:10 +0800

    [demangle] Fix types conflict for 'xmalloc' and 'xrealloc'

--- cmake/cctools/demangle/demangle.c
+++ cmake/cctools/demangle/demangle.c
@@ -1,5 +1,14 @@
-#include <stddef.h>
 #include <demangle.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+char *xmalloc PARAMS((unsigned size)) {
+    return malloc(size);
+}
+
+char *xrealloc PARAMS((char *mem, unsigned size)) {
+    return realloc(mem, size);
+}
 
 char * __cxa_demangle(const char *mangled_name, char *output_buffer, size_t *length, int *status) {
     return cplus_demangle(mangled_name, 0);
